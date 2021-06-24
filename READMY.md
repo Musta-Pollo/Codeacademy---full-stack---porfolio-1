63c8fce7e7a7799071b65ca7235d202343c1c98b
Jan Zimola <zimolajan789@gmail.com>
2021-06-24 20:58:34 +0200

    get random words

--- READMY.md
+++ READMY.md
@@ -7,8 +7,9 @@
 
 ## Todo's
 
-- [ ] Get random text
-- [ ] Convert text to array of words
-  - [ ] split
-  - [ ] filter (,[],...)
+- [x] Get random text
+- [x] Convert text to array of words
+  - [x] split
+  - [x] filter (,[],...)
 - [ ] Select 3 random words by index and print them in console
+
---
+++ main.js
@@ -0,0 +1,9 @@
+let randomText = "This rule is triggered when more than one space is used to separate the heading text from the hash characters in a closed atx style heading"
+
+let randomWords = randomText.split(' ').filter(word => word.length > 1)
+console.log(randomWords)
+
+
+const randomIndex = () => {
+    
+}
