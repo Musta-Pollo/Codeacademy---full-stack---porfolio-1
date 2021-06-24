8e9a14357497d98a0035f6ada9273309378821e1
Jan Zimola <zimolajan789@gmail.com>
2021-06-24 21:04:53 +0200

    Project goal completed

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+//Functions
+const randomIndex = (len) => {
+    return Math.floor(Math.random() * len)
+}
+
+
+//Main function
 let randomText = "This rule is triggered when more than one space is used to separate the heading text from the hash characters in a closed atx style heading"
 
-let randomWords = randomText.split(' ').filter(word => word.length > 1)
-console.log(randomWords)
+let randomWords = randomText.split(' ').filter(word => word.length > 1).map(word => word.toLowerCase())
+
+const message = `${randomWords[randomIndex(randomWords.length)]} ${randomWords[randomIndex(randomWords.length)]} ${randomWords[randomIndex(randomWords.length)]}`
+console.log(message)
 
 
-const randomIndex = () => {
-    
-}
